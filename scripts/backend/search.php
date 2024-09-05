09f8e0c9739d708623d0cda32f8f30d1a0bbc717
C-gana <patiencebanda91@gmail.com>
2024-09-05 12:43:09 +0200

    highliting the search ressult's search term fine tuning search.php and search.js

--- scripts/backend/search.php
+++ scripts/backend/search.php
@@ -2,15 +2,24 @@
 include_once "conn.php";
 $term = mysqli_real_escape_string($conn, $_POST['term']);
 $output = "";
+$re = [];
 $sql = "SELECT * from packages where destination LIKE '%{$term}%'";
 $result = $conn->query($sql);
 if($term != ""){
-  if($result->num_rows>0){
+  if($result->num_rows>0){        #if the search term is found
       foreach($result as $place){
-      $output .= "<div class='item'><a>{$place['destination']}</a></div>";
+      $set = $place['destination'];
+      array_push($re, $set);     #appending the result into the array
       }
   }else{
-    $output= "<p>No results matching \"$term\" !</p>";
+    $output= "not found";   
   }
-  echo $output;
+  
+}
+
+#responding to the ajax call
+if($output){
+  echo json_encode($output);
+}else{
+  echo json_encode($re);
 }
--- scripts/js/search.js
+++ scripts/js/search.js
@@ -13,12 +13,34 @@ searchInput.onkeyup = () => {
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send("term=" + term);
     xhr.onload = () => {
-      const data = xhr.response;
-      results.innerHTML = data;
+      const data = JSON.parse(xhr.response);
+      let dataHtml = ``;
+      if (typeof data == "object") {
+        // calling a function to highlight the search term from the result
+        dataHtml = highliter(term, data);
+      } else {
+        dataHtml = `<p>No results matching \"${term}\" !</p>`;
+        console.log(typeof data);
+      }
+      results.innerHTML = dataHtml;
       results.classList.remove("hidden");
     };
   }
 };
+
+// highlighting the result search term
+function highliter(term, ...data) {
+  const re = new RegExp(term, "g");
+  let newHtml = ``;
+  for (let i = 0; i < data.length; i++) {
+    data[i].forEach((k) => {
+      const highlited = k.replace(re, `<mark>${term}</mark>`);
+      newHtml += `<div class="item"><a>${highlited}</a></div>`;
+    });
+  }
+  return newHtml;
+}
+
 // listening on input to check if its not focused
 searchInput.onblur = () => {
   searchInput.value = "";
